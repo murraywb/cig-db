1c26bc08966843b0fd7b0712ed3de7871fdc7396
murraywb <murraywb19@gmail.com>
2024-10-15 16:04:05 +0100

    gunicorn to requirements

--- cig_db/flask-quotes-app/config.py
+++ cig_db/flask-quotes-app/config.py
@@ -1,7 +1,13 @@
 import os
+import re
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@localhost/book_quotes'
+    # Get the DATABASE_URL from Render or fall back to local Postgres
+    uri = os.getenv('DATABASE_URL') or 'postgresql://postgres:password@localhost/book_quotes'
+    
+    # Fix for Heroku-style `postgres://` URL prefix
+    if uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
